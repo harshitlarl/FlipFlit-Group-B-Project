a76cf4ca3cec08d8a8a4df425edbecae594ce630
harshitlarl <harshit.soni1@BLRETV-QPYCC470.local>
2023-09-18 12:59:12 +0530

    Added Comments in Java

--- src/com/flipkart/dao/UpdatePasswordDAOInterface.java
+++ src/com/flipkart/dao/UpdatePasswordDAOInterface.java
@@ -1,13 +1,46 @@
-
 package com.flipkart.dao;
 
 import com.flipkart.bean.GymOwner;
 import com.flipkart.bean.User;
 
 public interface UpdatePasswordDAOInterface {
-    public void updateGymOwnerPassword(String email,String password, String updatedPassword);
-    public void updateGymUserPassword(String email,String password, String updatedPassword);
-    public boolean verifyGymOwnerPassword(String email,String password);
-    public boolean verifyGymUserPassword(String email,String password);
+    /**
+     * Update the password of a gym owner.
+     *
+     * @param email           - The email of the gym owner
+     * @param password        - The current password of the gym owner
+     * @param updatedPassword - The new password to set
+     * @author: Harshit, Avinash
+     */
+    public void updateGymOwnerPassword(String email, String password, String updatedPassword);
 
+    /**
+     * Update the password of a gym user.
+     *
+     * @param email           - The email of the gym user
+     * @param password        - The current password of the gym user
+     * @param updatedPassword - The new password to set
+     * @author: Harshit, Avinash
+     */
+    public void updateGymUserPassword(String email, String password, String updatedPassword);
+
+    /**
+     * Verify the password of a gym owner.
+     *
+     * @param email    - The email of the gym owner
+     * @param password - The password to verify
+     * @return true if the password is verified, false otherwise
+     * @author: Harshit, Avinash
+     */
+    public boolean verifyGymOwnerPassword(String email, String password);
+
+    /**
+     * Verify the password of a gym user.
+     *
+     * @param email    - The email of the gym user
+     * @param password - The password to verify
+     * @return true if the password is verified, false otherwise
+     * @author: Harshit, Avinash
+     */
+    public boolean verifyGymUserPassword(String email, String password);
 }
